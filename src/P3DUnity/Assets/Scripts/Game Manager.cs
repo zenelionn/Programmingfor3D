4c0618aa1e37292dabe0387ac86f569f0a9fafaf
zenelionn <zenelionn@gmail.com>
2024-11-28 18:10:38 +0000

    Piecing things togther - barriers set up so player doesnt leave

---
+++ src/P3DUnity/Assets/Scripts/Game Manager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text playerText;
+    [SerializeField] private GameObject frontDoorBarrier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other){
+        // change text
+        if (other.gameObject.CompareTag("Player")){
+            Debug.Log("hitting");
+            playerText.text = "I'm pretty certain the pillow is in my room";
+             playerText.gameObject.SetActive(true);
+        }
+        
+
+    }
+
+    private void OnTriggerExit(Collider other){
+        if (other.gameObject.CompareTag("Player")){
+            
+             playerText.gameObject.SetActive(false);
+        }
+    }
+}
+
+
+// if approaching the front door barrier or the back door barrier:
+        // show up player canvas saying:
+        // "I'm pretty certain the pillow is in my room"
